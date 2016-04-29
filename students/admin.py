d7b1f83acc4cf74f923f1fbdab400f1ec612e6d4
antik <willburninhell@gmail.com>
2016-04-29 18:47:09 +0300

    HW8: Admin tasks done

--- students/admin.py
+++ students/admin.py
@@ -25,6 +25,17 @@ class StudentFormAdmin(ModelForm):
         return self.cleaned_data['student_group']
 
 
+class GroupFormAdmin(ModelForm):
+
+    def clean_leader(self):
+        # print leader
+        # print self.instance
+        leader = self.cleaned_data['leader']
+        if leader.student_group != self.instance:
+            raise ValidationError(u"Студент навчається в іншій групі.", code='invalid')
+        return self.cleaned_data['leader']
+
+
 class StudentAdmin(admin.ModelAdmin):
     list_display = ['last_name', 'first_name', 'card', 'student_group']
     list_display_links = ['last_name', 'first_name']
@@ -35,14 +46,40 @@ class StudentAdmin(admin.ModelAdmin):
     search_fields = [
         'last_name', 'first_name', 'middle_name', 'card', 'notes']
     form = StudentFormAdmin
+    actions = ['copy']
 
     def view_on_site(self, obj):
         return reverse('students_edit', kwargs={'pk': obj.id})
 
+    def copy(self, request, queryset):
+        for obj in queryset:
+            obj.pk = None
+            obj.save()
+        if len(queryset) == 1:
+            message = "Запис про студента"
+        else:
+            message = "Записи про студентів"
+        self.message_user(request, "%s було успішно клоновані." % message)
+
+    copy.short_description = "Клонувати виділені записи"
+
+
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ['title', 'leader']
+    # list_display_links = ['title']
+    list_editable = ['leader']
+    ordering = ['title']
+    list_per_page = 10
+    search_fields = ['title', 'leader__first_name', 'leader__last_name']
+    form = GroupFormAdmin
+
+    # def view_on_site(self, obj):
+    #     return reverse('students_edit', kwargs={'pk': obj.id})
+
 
 # Register your models here.
 admin.site.register(Student, StudentAdmin)
-admin.site.register(Group)
+admin.site.register(Group, GroupAdmin)
 admin.site.register(JournalEntry)
 admin.site.register(Exam)
 admin.site.register(Examresult)
